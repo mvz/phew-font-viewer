d3514d7236a84aeca4dc663f6da12416f2b30eca
Matijs van Zuijlen <matijs@matijs.net>
2012-04-12 07:33:14 +0200

    Show scripts, sample languages and sample texts.

--- pango_test.rb
+++ pango_test.rb
@@ -22,3 +22,12 @@ cov = fnt.get_coverage lang
 sample_cov = lang.get_sample_string.each_codepoint.map {|cp| cov.get cp}
 mostly = [:none, :fallback, :coverage, :exact].max_by {|i| sample_cov.count(i)}
 puts "Coverage mostly: #{mostly}"
+
+Pango::Script.symbols.each do |script|
+  lang = Pango.script_get_sample_language script
+  unless lang.nil?
+    puts "#{script}: #{lang.to_string}; #{lang.get_scripts.join ', '}, #{lang.get_sample_string}"
+  else
+    puts script
+  end
+end
