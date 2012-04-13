a480ccce7c5b1cb4ed08a809a821391936bb8190
Matijs van Zuijlen <matijs@matijs.net>
2012-04-13 17:47:03 +0200

    Give Pango test script a GUI.
    The test script now examines:
    - Adding an event handler to use Ctrl-Q to quit.
    - Showing a combo box with scripts.
    - Reacting to a change in the selected script.

--- pango_test.rb
+++ pango_test.rb
@@ -1,33 +1,68 @@
+require 'ffi-gtk3'
 require 'gir_ffi-pango'
 
-GirFFI.setup :Gdk
-Gdk.init []
+Gtk.init
 
-ctx = Gdk.pango_context_get
-fontmap = ctx.get_font_map
+if false
+  ctx = Gdk.pango_context_get
+  fontmap = ctx.get_font_map
 
-lang = Pango::Language.from_string "ja"
-sample = lang.get_sample_string
-puts "Language: #{lang.to_string}"
-puts "Scripts: #{lang.get_scripts.join ', '}"
-puts "Sample: #{sample}"
+  lang = Pango::Language.from_string "ja"
+  sample = lang.get_sample_string
+  puts "Language: #{lang.to_string}"
+  puts "Scripts: #{lang.get_scripts.join ', '}"
+  puts "Sample: #{sample}"
 
-fd = Pango::FontDescription.new
-fd.set_family "TakaoMincho"
+  fd = Pango::FontDescription.new
+  fd.set_family "TakaoMincho"
 
-fnt = fontmap.load_font ctx, fd
-puts "Font: #{fnt.describe.to_string}"
-cov = fnt.get_coverage lang
+  fnt = fontmap.load_font ctx, fd
+  puts "Font: #{fnt.describe.to_string}"
+  cov = fnt.get_coverage lang
 
-sample_cov = lang.get_sample_string.each_codepoint.map {|cp| cov.get cp}
-mostly = [:none, :fallback, :coverage, :exact].max_by {|i| sample_cov.count(i)}
-puts "Coverage mostly: #{mostly}"
+  sample_cov = lang.get_sample_string.each_codepoint.map {|cp| cov.get cp}
+  mostly = [:none, :fallback, :coverage, :exact].max_by {|i| sample_cov.count(i)}
+  puts "Coverage mostly: #{mostly}"
+end
+
+win = Gtk::Window.new :toplevel
+win.signal_connect 'destroy' do
+  Gtk.main_quit
+  true
+end
+
+win.signal_connect 'key-press-event' do |wdg, evt, ud|
+  if evt.state == :control_mask
+    if evt.keyval == "q".ord
+      win.destroy
+    end
+  end
+  false
+end
+
+vbox = Gtk::VBox.new false, 0
+win.add vbox
+
+combo = Gtk::ComboBoxText.new
+vbox.pack_start combo, false, false, 0
 
 Pango::Script.symbols.each do |script|
-  lang = Pango.script_get_sample_language script
-  unless lang.nil?
-    puts "#{script}: #{lang.to_string}; #{lang.get_scripts.join ', '}, #{lang.get_sample_string}"
-  else
-    puts script
-  end
+  next if script == :invalid_code
+  str = script.to_s
+  txt = str.gsub(/_/, ' ')
+  combo.append str, txt
 end
+
+combo.signal_connect 'changed' do
+  script = combo.active_text.gsub(/ /, '_').to_sym
+  lang = Pango.script_get_sample_language script
+
+  msg = "Changed to #{script.inspect}"
+  unless lang.nil?
+    msg << "; #{lang.to_string}; #{lang.get_scripts.join ', '}, #{lang.get_sample_string}"
+  end
+  puts msg
+end
+
+win.show_all
+Gtk.main
