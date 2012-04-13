5ea4babf8b94126e294ff723cc8eba311e0a92c4
Matijs van Zuijlen <matijs@matijs.net>
2012-04-13 17:38:45 +0200

    Enhance Atspi test script with recursive inspection and actions.

--- atspi_test.rb
+++ atspi_test.rb
@@ -2,15 +2,52 @@ require 'gir_ffi'
 
 GirFFI.setup :Atspi
 
+class Foo < GObject::Object
+  include Atspi::Action
+end
+
+module Atspi
+  load_class :Accessible
+  class Accessible
+    def each_child
+      child_count.times do |idx|
+        child = get_child_at_index idx
+        yield child
+      end
+    end
+
+    def inspect_recursive level=0, maxlevel=4
+      each_child do |child|
+        act = child.action
+        if act
+          act = Foo.wrap(act.to_ptr)
+          nactions = act.get_n_actions
+          actlist = nactions.times.map do |nth|
+            "#{act.get_description nth} <#{act.get_key_binding nth}>"
+          end
+          actions = "; actions: [#{actlist.join ', '}]"
+        else
+          actions = ""
+        end
+        puts "#{'  ' * level} > name: #{child.name}; role: #{child.role}#{actions}"
+        child.inspect_recursive(level + 1) unless level >= maxlevel
+      end
+    end
+  end
+end
+
 desktop = Atspi.get_desktop(0)
 
-p desktop.name
-p desktop.role
-p desktop.role_name
-p desktop.child_count
+desktop.inspect_recursive
 
-cnt = desktop.child_count
-(0..cnt-1).each do |i|
-  child = desktop.get_child_at_index i
-  p child.name
+desktop.each_child do |app|
+  if app.name == 'gnome-shell'
+    child = app.get_child_at_index 0
+    act = child.action
+    nactions = act.get_n_actions
+    puts "#{child.name} has #{nactions} actions"
+    nactions.times do |nth|
+      puts "Action #{nth} is #{act.get_description nth} with keybinding #{act.get_key_binding nth}."
+    end
+  end
 end
