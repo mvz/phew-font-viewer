034e8f581aeca320eb420078dd818e910177906d
Matijs van Zuijlen <matijs@matijs.net>
2012-04-15 15:08:36 +0200

    Create PhewDriver to drive GUI testing.

--- test/end_to_end/basic_run_test.rb
+++ test/end_to_end/basic_run_test.rb
@@ -4,25 +4,16 @@ require 'gir_ffi'
 
 GirFFI.setup :Atspi
 
-def find_app name
-  desktop = Atspi.get_desktop(0)
-  cnt = desktop.child_count
-
-  # We expect the app to be the last one.
-  (0..cnt-1).reverse_each do |i|
-    child = desktop.get_child_at_index i
-    return child if child.name == name
+def each_child acc
+  acc.child_count.times do |i|
+    yield acc.get_child_at_index i
   end
-  return nil
 end
 
-def each_child acc
-  cnt = acc.child_count
-
-  (0..cnt-1).reverse_each do |i|
-    child = acc.get_child_at_index i
-    yield child
-  end
+def find_app name
+  desktop = Atspi.get_desktop(0)
+  each_child(desktop) {|child| return child if child.name == name }
+  nil
 end
 
 def try_repeatedly
@@ -35,49 +26,62 @@ def try_repeatedly
   yield
 end
 
-def running_command command, timeout = 10
-  pid = Process.spawn command
+def press_ctrl_q
+  Atspi.generate_keyboard_event(37, nil, :press)
+  Atspi.generate_keyboard_event(24, nil, :pressrelease)
+  Atspi.generate_keyboard_event(37, nil, :release)
+end
 
-  killed = false
-
-  Thread.new do
-    sleep timeout
-    killed = true if pid
-    Process.kill "TERM", pid if pid
-    sleep 1 if pid
-    Process.kill "KILL", pid if pid
+class PhewDriver
+  def initialize
+    @app_file = File.expand_path('../../bin/phew', File.dirname(__FILE__))
+    @pid = nil
+    @killed = false
   end
 
-  begin
-    yield pid
-    _, status = Process.wait2 pid
-    pid = nil
-    raise "Process under test was killed" if killed
-    return status
-  ensure
-    Process.wait if pid
+  def boot timeout=3
+    raise "Already booted" if @pid
+    @pid = Process.spawn "ruby #@app_file"
+
+    @killed = false
+
+    Thread.new do
+      sleep timeout
+      @killed = true if pid
+      Process.kill "TERM", @pid if @pid
+      sleep 1 if @pid
+      Process.kill "KILL", @pid if @pid
+    end
+  end
+
+  def cleanup expected_status = 0
+    return unless @pid
+    _, status = Process.wait2 @pid
+    @pid = nil
+    @killed.must_equal false
+    status.exitstatus.must_equal 0
   end
 end
 
 describe "The Phew application" do
   before do
-    @app_file = File.expand_path('../../bin/phew', File.dirname(__FILE__))
+    @driver = PhewDriver.new
+    @driver.boot
   end
 
   it "starts and can be quit with Ctrl-q" do
-    status = running_command "ruby #@app_file" do
-      acc = try_repeatedly { find_app "phew" }
-      acc.wont_be_nil
+    acc = try_repeatedly { find_app "phew" }
+    acc.wont_be_nil
 
-      frame = acc.get_child_at_index 0
-      frame.role.must_equal :frame
+    frame = acc.get_child_at_index 0
+    frame.role.must_equal :frame
+    frame.grab_focus
+    sleep 0.01
 
-      frame.grab_focus
-      sleep 0.01
-      Atspi.generate_keyboard_event(37, nil, :press)
-      Atspi.generate_keyboard_event(24, nil, :pressrelease)
-      Atspi.generate_keyboard_event(37, nil, :release)
-    end
-    status.exitstatus.must_equal 0
+    press_ctrl_q
+  end
+
+  after do
+    @driver.cleanup
   end
 end
